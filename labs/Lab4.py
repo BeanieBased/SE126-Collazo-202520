92ee1dd2cc97fb9772cf3c4dd1d3ef7f95856f5a
BeanieBased <jcollazo@email.neit.edu>
2025-02-03 09:12:42 -0500

    Lab 4

--- labs/Lab4.py
+++ labs/Lab4.py
@@ -38,41 +38,62 @@ with open ("textFiles/got_emails.csv") as csvfile:
         email.append(f"{rec[3]}@westeros.net")
 
 #disconnected from file
+starkNum = 99
+targNum = 199
+tullyNum = 299
+lanNum = 399
+baraNum = 499
+nightNum = 599
 
 for i in range(0, len(fName)):
 
     if houseAlleg[i] == "House Stark":
         department.append("Research & Development")
-        phoneExt.append()
+        starkNum += 1
+        phoneExt.append(starkNum)
 
     
     elif houseAlleg[i] == "House Targaryen":
         department.append("Marketing")
-        phoneExt.append()
+        targNum += 1
+        phoneExt.append(targNum)
     
     elif houseAlleg[i] == "House Tully":
         department.append("Human Resources")
-        phoneExt.append()
+        tullyNum += 1
+        phoneExt.append(tullyNum)
     
     elif houseAlleg[i] == "House Lannister":
         department.append("Accounting")
-        phoneExt.append()
+        lanNum += 1
+        phoneExt.append(lanNum)
 
     elif houseAlleg[i] == "House Baratheon":
         department.append("Sales")
-        phoneExt.append()
+        baraNum += 1
+        phoneExt.append(baraNum)
     
     elif houseAlleg[i] == "The Night's Watch":
         department.append("Auditing")
-        phoneExt.append()
+        nightNum += 1
+        phoneExt.append(nightNum)
     
     else:
         department.append("ERROR")
         phoneExt.append("ERROR")
 
 print(f"{'FIRST':8} {'LAST':10} {'EMAIL':30} {'DEPARTMENT':23} {'EXT':3}")
-print("-------------------------------------------------------------------")
+print("--------------------------------------------------------------------------------------")
 for i in range(0, len(fName)):
-    print(f"{fName[i]:8} {lName[i]:10} {email[i]:30} {department[i]:23}")
-print("-------------------------------------------------------------------")
+    print(f"{fName[i]:8} {lName[i]:10} {email[i]:30} {department[i]:23} {phoneExt[i]:3}")
+print("--------------------------------------------------------------------------------------")
 
+
+#WRITE SOME DATA TO A NEW FILE
+#create and write all of the data to a new text file:
+file = open('textFiles/westeros.csv', 'w')
+
+for i in range(0,len(fName)):
+    file.write(f"{fName[i]:8} {lName[i]:10} {email[i]:30} {department[i]:23} {phoneExt[i]:3}\n")
+
+file.close()
