1d70ca9611d9e18549f10a0261795688c5fb7655
BeanieBased <jcollazo@email.neit.edu>
2025-02-25 09:52:29 -0500

    W8D2

---
+++ demos/W8D2_dictionariesAndTextFiles.py
@@ -0,0 +1,65 @@
+#W8D2 - Dictionaries and Text File Data
+
+#this demo utilizes: dictionary_file.csv [W8 Canvas Module]
+
+
+#--Imports------------------------------------------
+import csv
+#--MAIN EXECUTING CODE------------------------------
+
+#mini review on dictionaries
+library = {
+    #indexes are STRINGS set by the developer
+    #'key' : value
+    "1230" : "Red Rising",
+    "1231" : "The Little Prince"
+}
+
+libraryNums = []
+
+with open("textFiles/dictionary_file.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        #add each record's data as a new key + value pair from the text file
+        #key --> rec[0] ; value --> rec[1]
+        library.update({rec[0] : rec[1]})
+        #when using .update() --> pass {'key' : value}
+        libraryNums.append(rec[0])
+
+#disconnect from file
+print(f"{'KEY':6}\t{'TITLE'}")
+print("-" * 50)
+for key in library:
+    #for every key (and value) pair found within the 'library' dictionary
+    print(f"{key:6}\t{library[key]}")
+print("-" * 50)
+
+#Sequential Search for a Title
+search = input("Enter the TITLE you are looking for: ")
+found = 0
+
+for key in library:
+    if search.lower() == library[key].lower():
+        #store the found title's location in the dictionary -->
+        found = key
+
+if found != 0:
+    print(f"\nKEY:{found:6}\tTITLE:{library[found]}")
+else:
+    print(f"\nYour search for {search} came up empty :[")
+
+
+#BINARY SEARCH for LIBRARY NUMBER (dictionary keys)
+#in order to binary search a set of keys you must FIRST ...
+
+
+min = 0
+max = len(libraryNums) -1
+mid = int((min + max) / 2)
+
+while min < max and search != libraryNums[mid]:
+    if search < libraryNums[mid]:
+        max = mid - 1
+    else:
+        
