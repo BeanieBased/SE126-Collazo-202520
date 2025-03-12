8f4732a5beb0cf0771ee93f4387de44fa82d9c03
BeanieBased <collazojustyn@gmail.com>
2025-03-11 22:49:09 -0400

    Update collazoFinal.py

--- final/collazoFinal.py
+++ final/collazoFinal.py
@@ -66,8 +66,13 @@ else:
 
 guessBoard = [['~'] * len(selectedBoard[0]) for _ in range(len(selectedBoard))] #makes the board displayed a bunch of "~", for the length of the selected board so it's the same size
 
-# Count the total ships (X) on the selected board
-totalShips = sum(row.count('X') for row in selectedBoard) #turn this into a sorting method
+totalShips = 0
+
+# Count the total ships on the selected board
+for row in selectedBoard:  #go through each row
+    for cell in row:  #go through each element in the row
+        if cell == 'X':
+            totalShips += 1  #go up in count if 'X' is found
 
 
 # Initialize hit counter
@@ -81,7 +86,7 @@ hits = 0
 
 #Actual Game
 while hits < totalShips:
-    row, col = map(int, input("Enter row and column [example: 1 6]: ").split()) #row, col sets the variables to row and col from the input when there is a space between, and map 
+    row, col = map(int, input("Enter row and column [example: 1 6]: ").split()) #row, col sets the variables to row and col from the input when there is a space between, and map is setting them all to an integer
     
     if selectedBoard[row][col] == 'X':
         print("Hit!")
@@ -94,3 +99,36 @@ while hits < totalShips:
     printGuess()
 
 print("You sank all the ships! You win!")
+
+
+
+
+#Reflection!
+
+#How did you arrive at your Final Project Program idea?
+
+#it was the first thing to come to mind when thinking of 2D lists that wasn't tic tac toe. 
+
+#How did you approach building this program?
+
+#I originally made the 5 boards, and then made a way to display the board. I then commented that out, and made the guessing program, diplaying a guess board for all the hits and misses
+
+#If working in a group, include information on how work was divided and who was responsible for what
+
+#I, Justyn, Did it it all
+
+#What did you do to test your program?
+
+#I ran it everytime I changed something, until it worked as intended.
+
+#What was the hardest part about building this program?
+
+#The guessing code, I had to look on some forums to not split it into 2 input statements to simulate real battleship like "A1"
+
+#What was the most enjoyable or fun part about building this program?
+
+#Making the boards
+
+#If you could go back and change something about the program or your approach to building it, what would you change?
+
+#I would let the user create their own board, and have an AI guess the board randomly.
